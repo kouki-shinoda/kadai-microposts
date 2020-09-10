6b9da8e375699cbb290ca206b2f8e2d1874b2992
kouki.shinoda <shinosora-i0204@ezweb.ne.jp>
2020-09-10 14:19:08 +0000

    likes

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,4 +2,11 @@ class Micropost < ApplicationRecord
   belongs_to :user
   
   validates :content, presence: true, length: { maximum: 255 }
+  
+   has_many :favorites
+   has_many :users, through: :favorites
+   #has_many :reverses_of_favorite, class_name: "Favorite" , foreign_key: "micropost_id"
+   #has_many :micropst, through: :reverses_of_favorite, source: :user
+  
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,26 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false }
   has_secure_password
   
+  # ユーザーが持っている投稿
   has_many :microposts
+  
   has_many :relationships
+  
+  # フォローしている人をまとめてるやつ
   has_many :followings, through: :relationships, source: :follow
+  
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
+  
+  # フォローしてくれている人をまとめたやつ
   has_many :followers, through: :reverses_of_relationship, source: :user
   
+  has_many :favorites
+  
+  # お気に入りしている投稿をまとめるやつ
+  has_many :favoritings, through: :favorites, source: :micropost
+  
+  #has_many :reverses_of_favorite, class_name: "Favorite" , foreign_key: "micropost_id"
+  #has_many :micropst, through: :reverses_of_favorite, source: :micropost
   def follow(other_user)
     unless self == other_user
       self.relationships.find_or_create_by(follow_id: other_user.id)
@@ -30,4 +44,20 @@ class User < ApplicationRecord
   def feed_microposts
     Micropost.where(user_id: self.following_ids + [self.id])
   end
+  
+  def favorite(micropost)
+      self.favorites.find_or_create_by(micropost_id: micropost.id)
+  end
+
+  def unfavorite(micropost)
+    favorite = self.favorites.find_by(micropost_id: micropost.id)
+    favorite.destroy if favorite
+  end
+  
+  def favoritings?(micropost)
+    self.favoritings.include?(micropost)
+  end
+  
+  
+  
 end
